f830347f9df1d0dc3430612f0f8df9e682d7aab3
user <seonwh0105@gmail.com>
2026-03-18 16:38:16 +0900

    3월 25일

--- src/src/Foo.java
+++ src/src/Foo.java
@@ -1,5 +1,17 @@
 public class Foo {
+    public static int sum(int n, int m) {
+        return n + m;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello, Foo!");
+        int i = 20;
+        int s;
+        char a;
+
+        int m;
+        s = sum(i, m = 10);
+        a = '?';
+        System.out.println(a);
+        System.out.println("Hello");
+        System.out.println(s);
     }
 }
